e785f00030b44b2b1975a3f7681845fe78119fbf
Parshant Garg <parshantgarg50101@gmail.com>
2026-09-20 22:30:52 +0530

    Fix backend ESLint flat config

---
+++ backend/eslint.config.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs',
+      globals: {
+        require: 'readonly',
+        module: 'readonly',
+        exports: 'readonly',
+        process: 'readonly',
+        console: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+      },
+    },
+    rules: {
+      semi: ['error', 'always'],
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  },
+];
